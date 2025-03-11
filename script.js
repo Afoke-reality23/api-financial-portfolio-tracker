066d456ba9678a18ca0f51ecd4498b7e0b4bc2c0
Reality <ighoafokereality1@gmail.com>
2025-03-11 02:11:22 +0100

    testing server connection not really important commit

--- script.js
+++ script.js
@@ -2,5 +2,20 @@ const btn = document.getElementById("btn");
 console.log(btn);
 
 btn.addEventListener("click", () => {
-  fetch("htt");
+  fetch("http://localhost:1998", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("it works");
+      } else {
+        console.log("request failed with status:", response.status);
+      }
+    })
+    .catch((error) => {
+      console.error("fetch error", error);
+    });
 });
