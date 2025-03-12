6fe4e7988c49c874aa128460df8ee7539e96e911
Reality <ighoafokereality1@gmail.com>
2025-03-12 18:22:30 +0100

    important first commit

--- script.js
+++ script.js
@@ -2,18 +2,25 @@ const btn = document.getElementById("btn");
 console.log(btn);
 
 btn.addEventListener("click", () => {
-  fetch("http://localhost:1998", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
+  fetch(
+    "http://localhost:1998/portfolio?user_id=3&asset_id=4&trans_type=SELL&trans_quantity=200&trans_price=1800",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
     .then((response) => {
       if (response.ok) {
         console.log("it works");
+        return response.json();
       } else {
         console.log("request failed with status:", response.status);
       }
+    })
+    .then((data) => {
+      console.log(data);
     })
     .catch((error) => {
       console.error("fetch error", error);
