4ccd3194c5c266192e8c29981f9787dfcb5ec2d5
Reality <ighoafokereality1@gmail.com>
2025-03-12 18:22:30 +0100

    important first commit

--- server.py
+++ server.py
@@ -2,133 +2,141 @@ import psycopg2
 from parserdb import config
 import socket
 import json
+from urllib.parse import urlparse,parse_qs
+from decimal import Decimal
 
-server=socket.socket()
-port=1998
-server_IP=''
-server.bind((server_IP,port))
+server = socket.socket()
+port = 1998
+server_IP = ''  
+server.bind((server_IP, port))
 server.listen()
-print('server is listining for connection !!!')
-cors_header=(
-    'HTTP/1.1 200 OK\r\n'
-    'Content-Type:application/json\r\n'
-    'Access-Control-Allow-Origin:*\r\n'
-    'Access-Control-Allow-Methods:GET,POST,OPTIONS\r\n'
-    'Access-Control-Allow-Headers:Content-Type\r\n'\
-    '\r\n'
+print('Server is listening for connections!!!')
+
+cors_header = (
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: application/json\r\n"
+    "Access-Control-Allow-Origin: *\r\n"
+    "Access-Control-Allow-Methods: GET, POST, OPTIONS\r\n"
+    "Access-Control-Allow-Headers: Content-Type\r\n"
+    "\r\n"  
 )
+preflight_headers=(
+    "HTTP/1.1 204 NO CONTENT\r\n"
+    "Access-Control-Allow-Methods:GET,POST,OPTIONS\r\n"
+    "Access-Control-Allow-Headers:Content-Type\r\n"
+    "Access-Control-Allow-Origin:*\r\n"
+    "Content-Type:applicatin/json\r\n"
+)
+
 def handle_connections():
     try:
         while True:
-            conn,addr=server.accept()
-            print(f"conn={conn} and addr={addr}")
-            response={'connect_status':'successfull'}
-            apiresp=json.dumps(response)
-            ful_resp=cors_header + '\r\n' + apiresp
-            conn.send(ful_resp.encode("utf-8"))
-            conn.close()
-            print(ful_resp)
+            conn, addr = server.accept()
+            request=conn.recv(1024).decode()
+            headers=request.splitlines()[0]
+            if headers.startswith('OPTIONS'):#Handle preflight request and cors header issues
+                conn.send(preflight_headers.encode('utf-8'))
+                continue
+            print('connected successfully')
+            handle_clients(conn,headers)
     except KeyboardInterrupt as error:
-            print(error)
-    finally:
-            server.close()
-
-
-handle_connections()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def connect_db():
-    connection=None
-    try:
-        db_param=config()
-        connection=psycopg2.connect(**db_param)
-        print(f'{db_param['user']} connected to database')
-        connection.autocommit=True
-        crs=connection.cursor()
-        table="select table_name from information_schema.tables where table_schema='general'"
-        select_table='select * from portfolio'
-        trans_value={'user_id':3,'asset_id':3,'trans_type':'SELL','trans_quantity':-280,'trans_price':45000,'update':'INSERT'}
-        transaction_func()
-    except (Exception,psycopg2.DatabaseError) as error:
         print(error)
     finally:
-        if connection is not None:
-            connection.close()
-            print('database connection terminated')
+        conn.close()
+        server.close()
 
 
+def handle_clients(client_sock,headers):
+    try: 
+        data=url_parser(headers)
+        connect_db(data)
+        # print(db_response)
+        # id=0
+        # msg={'assets_id':'','assets_name':'','symbol':'','type':'','price':''}
+        # while id <= len(db_response):
+        #     msg.update(dict(zip(msg.keys(),db_response[id])))
+        #     msg={k:str(v) if isinstance(v,Decimal) else v for k,v in msg.items()}
+        #     id+=1
+        #     print(msg)
+        # complete_response=cors_header + json.dumps(msg)
+        # client_sock.send(complete_response.encode('utf-8'))
+        # print('msg delivered successfully')
+    except Exception as error:
+        print(error)
 
+def url_parser(headers):
+    if headers.startswith(('GET','POST')):
+        url=headers.split(" ")[1]
+        parse_url=urlparse(url)
+        table_name=parse_url.path.replace('/'," ").strip()
+        query_param=parse_qs(parse_url.query)
+        data={'table_name':table_name,'columns':{k:v[0] for k,v in query_param.items()}}
+        return data
 
-def  transaction_func():
+def connect_db(data):
     try:
-        crs.execute('BEGIN')
-        transaction=f"""
-        INSERT INTO transaction(user_id,asset_id,trans_type,trans_quantity,trans_price)
-            VALUES({trans_value['user_id']},{trans_value['asset_id']},'{trans_value['trans_type']}',{trans_value['trans_quantity']},{trans_value['trans_price']})
-        """
-        crs.execute(transaction)
-        owned_quantity__query=f"""
-        select quantity from portfolio p
-        where p.user_id=3 and p.asset_id=3
-        """
-        crs.execute(owned_quantity__query)
-        result=crs.fetchall()
-        print(len(result),result)
-        owned_quantity=result[0][0] if result else 0
-        print(owned_quantity)
-        if trans_value['trans_type']=='SELL' and trans_value['trans_quantity'] > owned_quantity and result:
-            raise ValueError('cannot sell more than owned')
-        quantity_update=(
-            f"GREATEST({owned_quantity} - {trans_value['trans_quantity']})"
-            if trans_value['trans_type']=='SELL'
-            else f" {owned_quantity} + {trans_value['trans_quantity']}"
-        )
-        transaction=f"""
-        INSERT INTO portfolio(user_id,asset_id,quantity)
-            VALUES({trans_value['user_id']},{trans_value['asset_id']},{trans_value['trans_quantity']})
-            ON CONFLICT(user_id,asset_id)
-            DO UPDATE
-            SET quantity={quantity_update}
-        """
-        crs.execute(transaction)
-        delet_port="delete from portfolio where quantity=0"
-        crs.execute(delet_port)
-        crs.execute('COMMIT')
-    except ValueError as error:
-        crs.execute('ROLLBACK')
+        db_params=config()
+        connect=psycopg2.connect(**db_params)
+        print('data base connected successfully')
+        connect.autocommit=True
+        crs=connect.cursor()
+        transaction(data,crs )
+    except psycopg2.DatabaseError as error:
         print(error)
     finally:
         crs.close()
 
 
+def transaction(data,crs):
+    try:
+        crs.execute('BEGIN')
+        insert=f"""
+        INSERT INTO transaction(user_id,asset_id,trans_type,trans_quantity,trans_price)
+            VALUES({int(data['columns']['user_id'])},{int(data['columns']['asset_id'])},'{data['columns']['trans_type']}',{int(data['columns']['trans_quantity'])},{int(data['columns']['trans_price'])})
+        """
+        crs.execute(insert)
+        crs.execute('select * from transaction')
+        fetch_trans=crs.fetchall()
+        print('fetching from transaction')
+        print(fetch_trans)
+        port_quantity_query=f"""
+        SELECT quantity FROM portfolio WHERE user_id={int(data['columns']['user_id'])}
+        """
+        crs.execute(port_quantity_query)
+        quantity_response=crs.fetchall()
+        port_quantity= quantity_response[0][0] if quantity_response else 0
+        print(port_quantity)
+        print(type(data['columns']['trans_quantity']),data['columns']['trans_quantity'])
+        if data['columns']['trans_type'] =='SELL' and int(data['columns']['trans_quantity']) > port_quantity and quantity_response:
+            raise ValueError('cannnot sell more than owned')
+        print('i am here now')
+        quantity=(
+            port_quantity - int(data['columns']['trans_quantity'])
+            if data['columns']['trans_type']=='SELL'
+            else port_quantity + int(data['columns']['trans_quantity'])
+            )
+        print(quantity)
+        portfolio=f"""
+        INSERT INTO portfolio(user_id,asset_id,quantity)
+            VALUES({data['columns']['user_id']},{data['columns']['asset_id']},{data['columns']['trans_quantity']})
+            ON CONFLICT(user_id,asset_id)
+            DO UPDATE
+            SET quantity=GREATEST({quantity},0)
+        """
+        crs.execute(portfolio)
+        crs.execute('select * from portfolio')
+        fetch_port=crs.fetchall()
+        delete_port='delete from portfolio where quantity=0'
+        crs.execute(delete_port)
+        print('fetching from portfolio')
+        print(fetch_port)
+        print('portfolio executed')
+        crs.execute('COMMIT')
+    except Exception as error:
+        print(error)
+        crs.execute('ROLLBACK')
 
-# connect_db()
+
+def assets():
+    asset="select * from assets"
+handle_connections()
