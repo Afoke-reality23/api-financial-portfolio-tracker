48e41e49b07b44c34ac0765e3fcbd3080d7dc04b
Reality <ighoafokereality1@gmail.com>
2025-03-18 16:45:52 +0100

    done with filling of asset in asset table; on connectiong to the the server, the check if the asset table is empty not if empty fetch asset from the coinmarket api to fill asset table on subsequent call for asset is to be supplied by database

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 const btn = document.getElementById("btn");
-console.log(btn);
+const tale_data = document.querySelector(".id");
+const tale_body = document.querySelector(".tbody");
+console.log(tale_body);
 
 btn.addEventListener("click", () => {
-  fetch("http://localhost:1998/transaction?user_id=1&asset=Bitcoin", {
+  fetch("http://localhost:1998/assets", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
-    // body: JSON.stringify({
-    //   user_id: 1,
-    //   asset_id: 1,
-    //   trans_type: "SELL",
-    //   trans_quantity: 10,
-    //   trans_price: 100,
-    // })
   })
     .then((response) => {
       if (response.ok) {
@@ -25,8 +20,27 @@ btn.addEventListener("click", () => {
     })
     .then((data) => {
       console.log(data);
+      updateTable(data);
     })
     .catch((error) => {
       console.error("fetch error", error);
     });
 });
+
+function updateTable(datas) {
+  for (data of datas) {
+    const row = document.createElement("tr");
+    const coin_id = document.createElement("th");
+    coin_id.textContent = data.assets_id;
+    const coin_log_symbol = document.createElement("th");
+    coin_log_symbol.textContent = data.symbol;
+    const price = document.createElement("th");
+    price.textContent = data.price;
+    // const last24Hr = document.createElement("th");
+    row.appendChild(coin_id);
+    row.appendChild(coin_log_symbol);
+    row.appendChild(price);
+    // row.appendChild(last24Hr);
+    tale_body.appendChild(row);
+  }
+}
