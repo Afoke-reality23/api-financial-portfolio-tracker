a97cecdea804fcc875a390f1d3e65f47718756c7
Reality <ighoafokereality1@gmail.com>
2025-03-21 01:02:41 +0100

    started desgining the frontend though not today started yesterday dont even know why i dont commit everyday

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const btn = document.getElementById("btn");
-const tale_data = document.querySelector(".id");
-const tale_body = document.querySelector(".tbody");
-console.log(tale_body);
+const tableBody = document.querySelector(".tbody");
+console.log(tableBody);
+
+// const num = 138369849;
+// num3 = Math.floor(num, 2);
 
 btn.addEventListener("click", () => {
   fetch("http://localhost:1998/assets", {
@@ -28,19 +30,49 @@ btn.addEventListener("click", () => {
 });
 
 function updateTable(datas) {
-  for (data of datas) {
-    const row = document.createElement("tr");
-    const coin_id = document.createElement("th");
-    coin_id.textContent = data.assets_id;
-    const coin_log_symbol = document.createElement("th");
-    coin_log_symbol.textContent = data.symbol;
-    const price = document.createElement("th");
-    price.textContent = data.price;
-    // const last24Hr = document.createElement("th");
-    row.appendChild(coin_id);
-    row.appendChild(coin_log_symbol);
-    row.appendChild(price);
-    // row.appendChild(last24Hr);
-    tale_body.appendChild(row);
+  try {
+    for (data of datas) {
+      console.log(data);
+      const row = document.createElement("tr");
+      const id = document.createElement("th");
+      const marketCap = document.createElement("th");
+      const iconSymbol = document.createElement("div");
+      const symbol = document.createElement("span");
+      const icon = document.createElement("img");
+      const price = document.createElement("th");
+      const percentChange24h = document.createElement("th");
+      percentChange24h.textContent =
+        Number((data.percent_change_24h / 100).toPrecision(3)) + "%";
+      data.percent_change_24h;
+      // icon.src = data.logo;
+      id.textContent = data.id;
+      symbol.textContent = data.symbol;
+      price.textContent = Number(data.price.toPrecision(7)).toLocaleString(
+        "en-US",
+        { style: "currency", currency: "USD" }
+      );
+      iconSymbol.appendChild(icon);
+      iconSymbol.appendChild(symbol);
+      marketCap.appendChild(iconSymbol);
+      row.appendChild(id);
+      row.appendChild(marketCap);
+      row.appendChild(price);
+      row.appendChild(percentChange24h);
+      tableBody.appendChild(row);
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
+
+// function data.percent_change_24hvalues) {
+//   console.log(values);
+//   if (values > 1e12) {
+//     console.log("i am here ");
+//     const value = (values / 1e12).toPrecision(2) + "T";
+//     console.log(value);
+//   }
+//   // if (values >= 1e9) return (values / 1e12).toPrecision(2) + "B";
+//   // if (values >= 1e6) return (values / 1e6).toPrecision(3) + "M";
+//   // if (values >= 1e6) return (values / 1e5).toPrecision(3) + "k";
+// }
